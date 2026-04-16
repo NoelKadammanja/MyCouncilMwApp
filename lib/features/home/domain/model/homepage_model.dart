b762af681bd5cb915e901de2ff1c7bcbaaf8bddc
NoelKadammanja <noel.kadammanja@nicotechnologies.com>
2026-04-16 14:35:56 +0200

    cleaned unwanted files
    cleaned unwanted files

--- lib/features/home/domain/model/homepage_model.dart
+++ lib/features/home/domain/model/homepage_model.dart
@@ -1,30 +1,7 @@
 import 'package:get/get.dart';
-import 'package:local_govt_mw/features/home/domain/model/list_item_model.dart';
-import 'package:local_govt_mw/features/home/domain/model/list_portfolio_item_model.dart';
 import 'package:local_govt_mw/utill/images.dart';
 
-
 class HomepageModel {
-  RxList<ListportfolioItemModel> listportfolioItemList =
-      RxList.filled(5, ListportfolioItemModel('', '', '', '', '', ''));
-
-  RxList<ListItemModel> listItemList = RxList.filled(5, ListItemModel());
-
-  static List<ListportfolioItemModel> getPortfolioData() {
-    return [
-      ListportfolioItemModel(ImageConstant.imgEllipse2832, "NICO", "Insurance",
-          "\MK80,30", "1.80(+1.32%)", "up"),
-      ListportfolioItemModel(ImageConstant.imgNikeIcon, "NBS", "Banking",
-          "\MK111,05", "-2.85(0.32%)", "down"),
-      ListportfolioItemModel(ImageConstant.imgEllipse2832, "ICON", "Property",
-          "\MK80,30", "1.80(+1.32%)", "up"),
-      ListportfolioItemModel(ImageConstant.imgNikeIcon, "AIRTEL", "Telecomunications",
-          "\MK111,05", "-2.85(0.32%)", "down"),
-      ListportfolioItemModel(ImageConstant.imgEllipse2832, "ILLOVO", "Manufacturing",
-          "\MK80,30", "1.80(+1.32%)", "up"),
-      ListportfolioItemModel(ImageConstant.imgNikeIcon, "BHL", "Property",
-          "\MK111,05", "-2.85(0.32%)", "down"),
-    ];
-  }
+//
 }
-//Listportfolio1ItemModel(this.icon,this.title,this.subtitle,this.rate,this.progress,this.progressType);
+
--- lib/features/home/domain/model/list_portfolio_item_model.dart
+++
@@ -1,9 +0,0 @@
-class ListportfolioItemModel {
-  String? icon;
-  String? title;
-  String? subtitle;
-  String? rate;
-  String? progress;
-  String? progressType;
-  ListportfolioItemModel(this.icon,this.title,this.subtitle,this.rate,this.progress,this.progressType);
-}
